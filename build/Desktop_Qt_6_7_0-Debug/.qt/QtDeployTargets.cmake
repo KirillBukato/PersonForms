e3d13dfd9b3368612920cc154221bba841918d96
cpp_Vupsen <kirill.bukato06@gmail.com>
2024-04-22 13:03:53 +0300

    First commit

---
+++ build/Desktop_Qt_6_7_0-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,1 @@
+set(__QT_DEPLOY_TARGET_PersonForms_FILE /home/kirill/QtProjects/PersonForms/build/Desktop_Qt_6_7_0-Debug/PersonForms)
---
+++ build/Desktop_Qt_6_7_0-Release/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/kirill/QtProjects/PersonForms/build/Desktop_Qt_6_7_0-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/kirill/Qt/6.7.0/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/kirill/Qt/6.7.0/gcc_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/clang-16" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/clang++-16" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
