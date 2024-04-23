0be99c508536b4201c5834a355acbf4fc8cc9bb7
cpp_Vupsen <kirill.bukato06@gmail.com>
2024-04-23 21:22:44 +0300

    швл

--- PersonData.h
+++ PersonData.h
@@ -1,4 +1,31 @@
 #ifndef PERSONDATA_H
 #define PERSONDATA_H
 
+enum SkinColor {
+    light_skin,
+    dark_skin
+};
+
+enum EyeColor {
+    blue,
+    green,
+    brown
+};
+
+enum HairColor {
+    light_hair,
+    dark_hair
+};
+
+struct PersonData {
+    int height;
+    int weight;
+    int chest_girth;
+    int waist_girth;
+    int shoulder_girth;
+    SkinColor skin_color;
+    EyeColor eye_color;
+    HairColor hair_color;
+};
+
 #endif // PERSONDATA_H
